575ff5230fd1788cea00861922bc13234ec23a0d
tony <tony@tcmp.flemings.house>
2025-11-26 00:56:43 -0600

    Migrated database including applying new migrations

--- backend/mintcastiq/models.py
+++ backend/mintcastiq/models.py
@@ -3,10 +3,33 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+#   * Remove `# managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.utils import timezone
 
+class DimSet(models.Model):
+    set_id = models.AutoField(primary_key=True)
+    set_name = models.TextField()
+    publisher = models.TextField(blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True)
+    set_year = models.CharField(max_length=10, blank=True, null=True)
+    ssubset_name = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'dim_set'
+
+class DimParallel(models.Model):
+    parallel_id = models.AutoField(primary_key=True)
+    parallel_name = models.TextField()
+    parallel_type = models.TextField(blank=True, null=True)
+    print_run = models.IntegerField(blank=True, null=True)
+    set = models.ForeignKey('DimSet', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'dim_parallel'
 
 class DimCard(models.Model):
     card_id = models.AutoField(primary_key=True)
@@ -18,33 +41,20 @@ class DimCard(models.Model):
     set = models.ForeignKey('DimSet', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
-        managed = False
+        # # managed = False
         db_table = 'dim_card'
-
 
 class DimCardHash(models.Model):
     hash_id = models.AutoField(primary_key=True)
-    card = models.ForeignKey(DimCard, models.DO_NOTHING)
+    card = models.ForeignKey(DimCard, models.DO_NOTHING, blank=True, null=True)
     hash_value = models.TextField()
     hash_type = models.TextField()
     hash_position = models.TextField()
     created_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'dim_card_hash'
-
-
-class DimContributor(models.Model):
-    contributor_id = models.AutoField(primary_key=True)
-    username = models.TextField(unique=True)
-    role = models.TextField(blank=True, null=True)
-    join_date = models.DateField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'dim_contributor'
-
 
 class DimGrade(models.Model):
     grade_id = models.AutoField(primary_key=True)
@@ -54,45 +64,8 @@ class DimGrade(models.Model):
     overlay_ref = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'dim_grade'
-
-
-class DimParallel(models.Model):
-    parallel_id = models.AutoField(primary_key=True)
-    parallel_name = models.TextField()
-    parallel_type = models.TextField(blank=True, null=True)
-    print_run = models.IntegerField(blank=True, null=True)
-    set = models.ForeignKey('DimSet', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'dim_parallel'
-
-
-class DimSessions(models.Model):
-    user = models.ForeignKey('DimUsers', models.DO_NOTHING)
-    session_token = models.CharField(unique=True, max_length=255)
-    expires_at = models.DateTimeField()
-    created_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'dim_sessions'
-
-
-class DimSet(models.Model):
-    set_id = models.AutoField(primary_key=True)
-    set_name = models.TextField()
-    publisher = models.TextField(blank=True, null=True)
-    release_date = models.DateField(blank=True, null=True)
-    set_year = models.CharField(max_length=10, blank=True, null=True)
-    ssubset_name = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'dim_set'
-
 
 class DimUsers(models.Model):
     username = models.CharField(unique=True, max_length=50)
@@ -102,18 +75,40 @@ class DimUsers(models.Model):
     is_admin = models.BooleanField(blank=True, null=True)
     is_contributor = models.BooleanField(blank=True, null=True)
     invited = models.BooleanField(blank=True, null=True)
+    join_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    role = models.TextField(blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'dim_users'
+
+class DimSessions(models.Model):
+    user = models.ForeignKey('DimUsers', models.DO_NOTHING, blank=True, null=True)
+    session_token = models.CharField(unique=True, max_length=255)
+    expires_at = models.DateTimeField()
     created_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
-        db_table = 'dim_users'
+        # managed = False
+        db_table = 'dim_sessions'
 
+class FactChecklistItem(models.Model):
+    checklist_item_id = models.AutoField(primary_key=True)
+    card = models.ForeignKey(DimCard, models.DO_NOTHING)
+    set = models.ForeignKey(DimSet, models.DO_NOTHING)
+    parallel = models.ForeignKey(DimParallel, models.DO_NOTHING)
+    source = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
 
+    class Meta:
+        # managed = False
+        db_table = 'fact_checklist_item'
+        
 class FactCardEvents(models.Model):
     event_id = models.AutoField(primary_key=True)
-    card = models.ForeignKey(DimCard, models.DO_NOTHING)
-    contributor = models.ForeignKey(DimContributor, models.DO_NOTHING)
-    set = models.ForeignKey(DimSet, models.DO_NOTHING)
+    card = models.ForeignKey(DimCard, models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(DimUsers, models.DO_NOTHING, blank=True, null=True)
+    set = models.ForeignKey(DimSet, models.DO_NOTHING, blank=True, null=True)
     grade = models.ForeignKey(DimGrade, models.DO_NOTHING, blank=True, null=True)
     timestamp = models.DateTimeField(blank=True, null=True)
     action_type = models.TextField()
@@ -121,15 +116,14 @@ class FactCardEvents(models.Model):
     processing_time_ms = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'fact_card_events'
 
 
 class FactInventory(models.Model):
     inventory_id = models.AutoField(primary_key=True)
-    contributor = models.ForeignKey(DimContributor, models.DO_NOTHING)
-    card = models.ForeignKey(DimCard, models.DO_NOTHING)
-    set = models.ForeignKey(DimSet, models.DO_NOTHING)
+    user = models.ForeignKey(DimUsers, models.DO_NOTHING, blank=True, null=True)
+    checklist_item = models.ForeignKey(FactChecklistItem, models.DO_NOTHING, blank=True, null=True)
     grade = models.ForeignKey(DimGrade, models.DO_NOTHING, blank=True, null=True)
     quantity = models.IntegerField(blank=True, null=True)
     acquired_at = models.DateTimeField(blank=True, null=True)
@@ -137,17 +131,17 @@ class FactInventory(models.Model):
     notes = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'fact_inventory'
 
 
 class FactMetrics(models.Model):
     metric_id = models.AutoField(primary_key=True)
-    event = models.ForeignKey(FactCardEvents, models.DO_NOTHING)
+    event = models.ForeignKey(FactCardEvents, models.DO_NOTHING, blank=True, null=True)
     metric_name = models.TextField()
     metric_value = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
     unit = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'fact_metrics'
